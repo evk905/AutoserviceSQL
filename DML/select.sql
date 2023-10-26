fc81bc0c2e3cf3a00bdc5dac08ec536cd4382776
Ekaterina Kovalenko <evk_905@inbox.ru>
2023-10-26 22:55:13 +0300

    add selects with join

--- DML/select.sql
+++ DML/select.sql
@@ -12,4 +12,19 @@ SELECT DISTINCT c.name_carbrand AS brand, count(c.name_carbrand) AS count_cars F
  JOIN carmodel c2 ON c.id=c2.brand_id JOIN car c3 ON c2.id=c3.model_id  
  GROUP BY c.name_carbrand; --количество машин каждого бренда
 
+SELECT c.id AS "Номер клиента", c.first_name AS "Имя клиента", c.last_name AS "Фамилия клиента", o.id AS "Номер заказа", o.date_order AS "Дата заказа"  
+FROM customer c JOIN "order" o  ON o.customer_id =c.id
+WHERE o.status= true;
+--когда, какой клиент и какой заказ оформил
+
+SELECT c.id AS "Номер клиента", c.first_name AS "Имя клиента", c.last_name AS "Фамилия клиента", o.id AS "Номер заказа", e.id AS "Менеджер", o.date_order AS "Дата заказа"  
+FROM customer c JOIN "order" o  ON o.customer_id =c.id
+JOIN employee e ON e.id =o.employee_id 
+WHERE o.status= true
+--в дополнение к предыдущему запросу узнать, какой продавец обслуживал заказ
+
+SELECT c.id AS "Номер клиента", CONCAT(c.first_name, c.last_name) AS "Клиент", o.id AS "Номер заказа", CONCAT(e.first_name, e.last_name) AS "Менеджер", o.date_order AS "Дата заказа"  
+FROM customer c JOIN "order" o  ON o.customer_id =c.id
+JOIN employee e ON e.id =o.employee_id 
+WHERE o.status= true; -- или объединив имя и фамилии в один столбец
 
